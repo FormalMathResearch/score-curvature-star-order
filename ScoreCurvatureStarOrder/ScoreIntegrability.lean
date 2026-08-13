a9e192121c5c3fde5efe7ed4ebf386dfb3537797
FormalMathResearch <tfahlberg469@gmail.com>
2026-08-14 00:05:34 +0200

    Add score integrability module

---
+++ ScoreCurvatureStarOrder/ScoreIntegrability.lean
@@ -0,0 +1,10 @@
+import Mathlib
+import ScoreCurvatureStarOrder.Moments
+import ScoreCurvatureStarOrder.Boundary
+
+namespace ScoreCurvatureStarOrder
+
+open Set MeasureTheory Filter
+open scoped Interval Topology
+
+end ScoreCurvatureStarOrder
