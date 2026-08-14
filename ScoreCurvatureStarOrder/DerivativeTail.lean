8f91c1260bfddcf4979cc77c635f641a08d7d04d
FormalMathResearch <tfahlberg469@gmail.com>
2026-08-14 08:05:22 +0200

    Fix derivative tail elaboration

--- ScoreCurvatureStarOrder/DerivativeTail.lean
+++ ScoreCurvatureStarOrder/DerivativeTail.lean
@@ -35,7 +35,7 @@ theorem exists_scoreRatio_lt_score_on_tail
     intro z hz
     exact (hSpos z hz).ne'
   have hratio : AntitoneOn (fun z => Sprime z / S z) (Set.Ici R) :=
-    scoreRatio_antitoneOn convex_Ici hS_R hSprime_R hSnz hcurv_R
+    scoreRatio_antitoneOn (convex_Ici R) hS_R hSprime_R hSnz hcurv_R
   by_contra hcontra
   have hge : ∀ z ∈ Set.Ici R, S z ≤ Sprime z / S z := by
     intro z hz
@@ -71,11 +71,12 @@ theorem exists_scoreRatio_lt_score_on_tail
     exact le_add_of_nonneg_right (div_nonneg hdelta.le hc2.le)
   have hxmem : x ∈ Set.Ici R := hRx
   have hgrowth : c ^ 2 * (x - R) ≤ S x - S R :=
-    convex_Ici.mul_sub_le_image_sub_of_le_deriv
+    (convex_Ici R).mul_sub_le_image_sub_of_le_deriv
       hcont hdiff hderiv_lower_int R hRmem x hxmem hRx
   have hstep : c ^ 2 * (x - R) = delta := by
     dsimp [x]
     field_simp [hc2.ne']
+    ring
   have hq_lt_Sx : qR < S x := by
     rw [hstep] at hgrowth
     dsimp [delta] at hgrowth
