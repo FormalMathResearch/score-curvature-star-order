b9f224581672861228af0f107bb35ef3378d3e26
FormalMathResearch <tfahlberg469@gmail.com>
2026-08-15 10:11:25 +0200

    Import shift boundary continuity theorem

--- ScoreCurvatureStarOrder.lean
+++ ScoreCurvatureStarOrder.lean
@@ -8,6 +8,7 @@ import ScoreCurvatureStarOrder.GlobalKernel
 import ScoreCurvatureStarOrder.GlobalKernelWithin
 import ScoreCurvatureStarOrder.Tail
 import ScoreCurvatureStarOrder.Moments
+import ScoreCurvatureStarOrder.ShiftBoundaryContinuity
 import ScoreCurvatureStarOrder.Boundary
 import ScoreCurvatureStarOrder.ScoreIntegrability
 import ScoreCurvatureStarOrder.IntegrationByParts
